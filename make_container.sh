01b379c0871f31f67163151c4abeab3ef209b0ff
sujong <dgdgksj@gmail.com>
2024-03-07 16:37:16 +0900

    fix: GStreamer error in Docker container setup
    make_container.sh 업데이트 진행
    기존 쉘으로 컨테이너 생성을 진행할 경우 x11이 동작하지 않아
    video_stream에 문제가 발생했음
    수정을 통해 bug fix 진행

--- make_container.sh
+++ make_container.sh
@@ -14,28 +14,38 @@ TEMP_CONTAINER_NAME="temp_for_copy"
 # ?? ???? ?? (???? ??)
 docker rm -f $TEMP_CONTAINER_NAME || true
 
-# ? ???? ?? (???? ??)
+
 docker rm -f $NEW_CONTAINER_NAME || true
 
-# ?? ???? ??
+
 docker run -d --name $TEMP_CONTAINER_NAME $BASE_CONTAINER_IMAGE
 
-# /jetson-inference ???? ?? ? /webRTC_server ???? ??
+
 docker exec $TEMP_CONTAINER_NAME bash -c "rm -rf /jetson-inference && mkdir -p /webRTC_server"
 
-# ?? ????? ??? /webRTC_server? ??
+
 docker cp $CURRENT_DIR/. $TEMP_CONTAINER_NAME:/webRTC_server
 
-# ?? ??????? ? ??? ??
+
 COMMITTED_IMAGE=$(docker commit $TEMP_CONTAINER_NAME)
 
-# ?? ???? ??
+
 docker rm -f $TEMP_CONTAINER_NAME
 
-# ?? Dockerfile ??
+
 cat > Dockerfile.temp <<EOF
 FROM $COMMITTED_IMAGE
-WORKDIR /webRTC_server
+WORKDIR /webRTC_server/demo
+RUN echo "export SSL_KEY=/webRTC_server/asset/key.pem" >> /root/.bashrc && \
+    echo "export SSL_CERT=/webRTC_server/asset/cert.pem" >> /root/.bashrc
+RUN apt-get update && apt-get upgrade -y && \
+    apt-get install -y gstreamer1.0-x x11-apps cmake build-essential
+#WORKDIR /webRTC_server/build
+#RUN cmake ../ && make && make install && ldconfig
+
+WORKDIR /webRTC_server/demo
+
+
 EOF
 
 # Dockerfile? ???? ?? ??? ??
@@ -45,5 +55,5 @@ docker build -f Dockerfile.temp -t $NEW_IMAGE_NAME .
 rm Dockerfile.temp
 
 # ? ???? ??
-docker run -it --name $NEW_CONTAINER_NAME $NEW_IMAGE_NAME
+docker run -it --privileged --name $NEW_CONTAINER_NAME $NEW_IMAGE_NAME
 
